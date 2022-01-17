79c06745af578438cb885b482813c13e55ed5919
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-01-17 18:44:48 -0500

    added form to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,6 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler)
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-
+buttonEl.addEventListener("click", createTaskHandler);
+  
 
